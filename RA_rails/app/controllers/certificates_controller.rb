81b05286143fe70af252e6a1f55cf499cb333e9c
Stephan Toggweiler <stephan.toggweiler@gmail.com>
2011-08-06 11:49:17 +0200

    remove old files

--- RA_rails/app/controllers/certificates_controller.rb
+++
@@ -1,7 +0,0 @@
-class CertificatesController < ApplicationController
-  before_filter :authenticate_user!
-  
-  def index
-  end
-  
-end
--- RA_rails/app/models/certificate.rb
+++
@@ -1,2 +0,0 @@
-class Certificate < ActiveRecord::Base
-end
