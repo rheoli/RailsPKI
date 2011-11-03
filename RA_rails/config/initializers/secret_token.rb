8b1f8ad0c67a3212e153acc74a243471ab0968a2
Stephan Toggweiler <stephan.toggweiler@gmail.com>
2011-11-03 08:19:52 +0100

    new rails 3.1.1 code

---
+++ RA_rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsPKI::Application.config.secret_token = 'bd80e6eb91bda1fc28035e979549189578464a002d655df13a05076fe46be4c204570caf9169ab26d4173a694c0f9a09760d16c9a4da4e21bcb6eef6db771e09'
