16bda3f13a8b025911d0eb386634fe4354bb1368
Stephan Toggweiler <stephan.toggweiler@gmail.com>
2011-11-03 08:19:29 +0100

    remove old rails data

--- RA_rails/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RailsPKI::Application.config.secret_token = 'c3de58f5373c16432ded18e4c36bd19596c5d767298f5b2a4dfc044e7d138ec05033d24f0c1081b1acd72edf29a448900b718cd20e9c57fc88c6e0da635d0728'
